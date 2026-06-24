454345af097175b40d80cc724f6b9e7ffa0ef3df
Guillaume <guillaume@local>
2026-06-24 16:47:35 +0200

    Journal : flux 'Changements récents' initial généré depuis git (12 rubriques créées)

--- journal-changements.js
+++ journal-changements.js
@@ -2,6 +2,103 @@
 // Ne pas éditer à la main : lancer gen_journal_changements.py qui lit
 // l'historique git des pages du journal et régénère ce fichier.
 window.JOURNAL_CHANGEMENTS = {
-  lastUpdated: null,
-  entries: []
+  "lastUpdated": "2026-06-24",
+  "entries": [
+    {
+      "author": "Guillaume",
+      "date": "2026-06-24",
+      "action": "créé",
+      "target": "Calendriers",
+      "type": "temps",
+      "page": "journal-calendriers.html"
+    },
+    {
+      "author": "Guillaume",
+      "date": "2026-06-24",
+      "action": "créé",
+      "target": "Cartes",
+      "type": "monde",
+      "page": "journal-cartes.html"
+    },
+    {
+      "author": "Guillaume",
+      "date": "2026-06-24",
+      "action": "créé",
+      "target": "Chronologies",
+      "type": "temps",
+      "page": "journal-chronologies.html"
+    },
+    {
+      "author": "Guillaume",
+      "date": "2026-06-24",
+      "action": "créé",
+      "target": "Créatures",
+      "type": "monde",
+      "page": "journal-creatures.html"
+    },
+    {
+      "author": "Guillaume",
+      "date": "2026-06-24",
+      "action": "créé",
+      "target": "Familles",
+      "type": "monde",
+      "page": "journal-familles.html"
+    },
+    {
+      "author": "Guillaume",
+      "date": "2026-06-24",
+      "action": "créé",
+      "target": "Journaux",
+      "type": "temps",
+      "page": "journal-journaux.html"
+    },
+    {
+      "author": "Guillaume",
+      "date": "2026-06-24",
+      "action": "créé",
+      "target": "Lieux",
+      "type": "monde",
+      "page": "journal-lieux.html"
+    },
+    {
+      "author": "Guillaume",
+      "date": "2026-06-24",
+      "action": "créé",
+      "target": "Nations",
+      "type": "monde",
+      "page": "journal-nations.html"
+    },
+    {
+      "author": "Guillaume",
+      "date": "2026-06-24",
+      "action": "créé",
+      "target": "Objets",
+      "type": "jeu",
+      "page": "journal-objets.html"
+    },
+    {
+      "author": "Guillaume",
+      "date": "2026-06-24",
+      "action": "créé",
+      "target": "Organisations",
+      "type": "monde",
+      "page": "journal-organisations.html"
+    },
+    {
+      "author": "Guillaume",
+      "date": "2026-06-24",
+      "action": "créé",
+      "target": "Personnages",
+      "type": "monde",
+      "page": "journal-personnages.html"
+    },
+    {
+      "author": "Guillaume",
+      "date": "2026-06-24",
+      "action": "créé",
+      "target": "Quêtes",
+      "type": "jeu",
+      "page": "journal-quetes.html"
+    }
+  ]
 };
